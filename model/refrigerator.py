4b543bb3521bc254fd6cdedc0ce031aecb6f581d
DESKTOP-8VERS95\user1 <y88.choi@gmail.com>
2026-08-04 17:13:29 +0900

    데이터 저장 형태를 json으로

--- model/refrigerator.py
+++ model/refrigerator.py
@@ -13,6 +13,14 @@ class Food:
     def __str__(self):
         return f"{self.name}, {self.price}원, {self.calories}칼로리(Kcal), 유통기한 {self.exp_date}"
 
+    def to_dict(self) -> dict:
+        return {"name": self.name, "price": self.price, "calories": self.calories, "exp_date": self.exp_date.isoformat()}
+
+    @staticmethod
+    def from_dict(food_data) -> Food:
+        exp_date = date.fromisoformat(food_data["exp_date"])
+        return Food(name = food_data["name"], price = food_data["price"], calories = food_data["calories"], exp_date = exp_date)
+
     def if_food_expired(self):
         if self.days_left < 0:
             return True
--- refrigerator_manager.py
+++ refrigerator_manager.py
@@ -1,11 +1,6 @@
+import json
 from datetime import date
 from model.refrigerator import Food
-
-refrigerator = []
-consumed_list = []
-garbage = []
-food_consumed_kcal = 0
-wasted_money = 0
 
 
 def main_menu():
@@ -25,98 +20,140 @@ def main_menu():
 
 # 1. 냉장고에 음식 넣기 // name, price, calories, exp_date)
 def food_in():
+    food_list = select_list()
     print("--------------- 냉장고에 음식 넣기 ---------------")
-    name = input("음식 이름 : ")
-    price = int(input("음식 가격(원) : "))
-    calories = int(input("음식 칼로리(Kcal) : "))
-    input_date = input("음식 유통기한(XXXX-XX-XX): ")
-    exp_date = date.fromisoformat(input_date)
-    food = Food(name, price, calories, exp_date)
-    refrigerator.append(food)
+    with open("food_data.json", "w", encoding="utf-8") as f:
+        name = input("음식 이름 : ")
+        price = int(input("음식 가격(원) : "))
+        calories = int(input("음식 칼로리(Kcal) : "))
+        input_date = input("음식 유통기한(XXXX-XX-XX): ")
+        exp_date = date.fromisoformat(input_date)
+        food = Food(name, price, calories, exp_date)
+        food_dict = food.to_dict()
+        food_list.append(food_dict)
+        json.dump(food_list, f, ensure_ascii=False, indent=2)
     print("------------- 냉장고에 음식 넣기 완료 ------------")
 
 
+def select_list():
+    with open("food_data.json", "r", encoding="utf-8") as f:
+        food_list = json.load(f)
+    return food_list
+
+
+def select_consumed_list():
+    with open("consumed_data.json", "r", encoding="utf-8") as f:
+        consumed_list = json.load(f)
+    return consumed_list
+
+
+def select_wasted_list():
+    with open("wasted_data.json", "r", encoding="utf-8") as f:
+        wasted_list = json.load(f)
+    return wasted_list
+
+
 # 2. 냉장고 음식 목록
-def refrigerator_food_list():
+def refrigerator_food_list(food_list):
     print("--------------- 냉장고 음식 목록 ---------------")
-    if len(refrigerator) < 1:
+    if len(food_list) < 1:
         print("냉장고가 비었습니다.")
         return
     print("번호 | 음식")
-    for idx, food in enumerate(refrigerator):
+    for idx, food in enumerate(food_list):
+        food = Food.from_dict(food)
         line = f"{idx}.  | {food.name}"
-        if refrigerator[idx].if_food_expired():
+        if food.if_food_expired():
             line += "  (상한음식)"
         print(line)
     print("----------- 냉장고 음식 목록 조회 완료 -----------")
 
 
 # 3. 음식 조회
-def food_detail():
+def food_detail(food_list):
     print("--------------- 냉장고 음식 상세 조회 ---------------")
     number = int(input("음식 번호 : "))
     print("번호 | 음식")
-    print(f"{number}.  | {refrigerator[number]}")
+    food = Food.from_dict(food_list[number])
+    print(f"{number}.  | {food}")
     print("------------ 냉장고 음식 상세 조회 완료 -------------")
 
 
 # 4. 음식 섭취
-def eat_food():
-    global food_consumed_kcal
+def eat_food(food_list):
     print("--------------- 냉장고 음식 섭취 ---------------")
     number = int(input("음식 번호 : "))
-    result = refrigerator[number].if_food_expired()
+    food = Food.from_dict(food_list[number])
+    result = food.if_food_expired()
     if result:
-        print(f"음식 : {refrigerator[number].name} / 유통기한 : {refrigerator[number].exp_date}")
+        print(f"음식 : {food.name} / 유통기한 : {food.exp_date}")
         print("유통기한이 지난 음식은 섭취 할수 없습니다.")
     else:
-        food_consumed_kcal += refrigerator[number].calories
-        consumed_list.append(refrigerator[number])
-        print(f"{refrigerator[number].name} 섭취로 획득한 칼로리 :  {refrigerator[number].calories} Kcal")
-        del refrigerator[number]
+        consumed_food_list = select_consumed_list()
+        with open("consumed_data.json", "w", encoding="utf-8") as f:
+            consumed_food = food
+            consumed_food_dict = consumed_food.to_dict()
+            consumed_food_list.append(consumed_food_dict)
+            json.dump(consumed_food_list, f, ensure_ascii=False, indent=2)
+            print(f"{food.name} 섭취로 획득한 칼로리 :  {food.calories} Kcal")
+            with open("food_data.json", "w", encoding="utf-8") as f:
+                del food_list[number]
+                json.dump(food_list, f, ensure_ascii=False, indent=2)
     print("------------- 냉장고 음식 섭취 완료 ------------")
 
 
 # 5. 음식 폐기
-def disposal_food():
-    global wasted_money
+def disposal_food(food_list):
     print("--------------- 상한 음식 폐기 ---------------")
     number = int(input("음식 번호 : "))
-    result = refrigerator[number].if_food_expired()
+    food = Food.from_dict(food_list[number])
+    result = food.if_food_expired()
     if result != True:
-        print(f"음식 : {refrigerator[number].name} / 유통기한 : {refrigerator[number].exp_date}")
+        print(f"음식 : {food.name} / 유통기한 : {food.exp_date}")
         print("유통기한이 남은 음식은 폐기 할수 없습니다.")
     else:
-        wasted_money += refrigerator[number].price
-        garbage.append(refrigerator[number])
-        print(f"{refrigerator[number].name} 폐기로 낭비된 금액 :  {refrigerator[number].price} 원")
-        del refrigerator[number]
+        wasted_food_list = select_wasted_list()
+        with open("wasted_data.json", "w", encoding="utf-8") as f:
+            wasted_food = food
+            wasted_food_dict = wasted_food.to_dict()
+            wasted_food_list.append(wasted_food_dict)
+            json.dump(wasted_food_list, f, ensure_ascii=False, indent=2)
+            print(f"{food.name} 폐기로 낭비된 금액 :  {food.price} 원")
+            with open("food_data.json", "w", encoding="utf-8") as f:
+                del food_list[number]
+                json.dump(food_list, f, ensure_ascii=False, indent=2)
     print("------------- 상한 음식 폐기 완료 ------------")
 
 
 # 6. 음식 섭취 리스트
-def food_consumed_list():
+def food_consumed_list(consumed_list):
     print("--------------- 섭취 음식 목록 ---------------")
     if len(consumed_list) < 1:
         print("섭취 음식이 없습니다.")
         return
     print("번호 |  음식 이름 / 칼로리(Kcal)")
+    food_consumed_kcal = 0
     for idx, food in enumerate(consumed_list):
-        print(f"{idx}.  |  {food.name} / {food.calories} Kcal")
+        consumed_food = Food.from_dict(food)
+        print(f"{idx}.  |  {consumed_food.name} / {consumed_food.calories} Kcal")
+        food_consumed_kcal += consumed_food.calories
     print(f"총 섭취 칼로리 :  {food_consumed_kcal} Kcal")
     print("----------- 섭취 음식 목록 조회 완료 -----------")
 
 
 # 7. 음식 폐기 리스트
-def food_wasted_list():
+def food_wasted_list(wasted_list):
     print("--------------- 폐기 음식 목록 ---------------")
-    if len(garbage) < 1:
+    if len(wasted_list) < 1:
         print("폐기 음식이 없습니다.")
         return
     print("번호 |  음식 이름 / 가격(원)")
-    for idx, food in enumerate(garbage):
-        print(f"{idx}.  |  {food.name} / {food.price} 원")
-    print(f"음식 폐기로 낭비된 총액 :  {wasted_money} 원")
+    food_wasted_price = 0
+    for idx, food in enumerate(wasted_list):
+        wasted_food = Food.from_dict(food)
+        print(f"{idx}.  |  {wasted_food.name} / {wasted_food.price} 원")
+        food_wasted_price += wasted_food.price
+    print(f"음식 폐기로 낭비된 총액 :  {food_wasted_price} 원")
     print("----------- 폐기 음식 목록 조회 완료 -----------")
 
 
@@ -126,17 +163,23 @@ def run_refrigerator_manager():
         case "1":
             food_in()
         case "2":
-            refrigerator_food_list()
+            food_list = select_list()
+            refrigerator_food_list(food_list)
         case "3":
-            food_detail()
+            food_list = select_list()
+            food_detail(food_list)
         case "4":
-            eat_food()
+            food_list = select_list()
+            eat_food(food_list)
         case "5":
-            disposal_food()
+            food_list = select_list()
+            disposal_food(food_list)
         case "6":
-            food_consumed_list()
+            consumed_list = select_consumed_list()
+            food_consumed_list(consumed_list)
         case "7":
-            food_wasted_list()
+            wasted_list = select_consumed_list()
+            food_wasted_list(wasted_list)
         case "8":
             auto_in()
         case "0":
@@ -147,19 +190,24 @@ def run_refrigerator_manager():
 
 #### 테스트/시연용 자동 음식 넣기.
 def auto_in():
-    foods = [
-        ("사과", 1500, 140, "2026-09-01"),
-        ("망고", 4500, 340, "2026-07-21"),
-        ("햄버거", 10000, 950, "2026-09-21"),
-        ("감자샐러드", 7500, 650, "2026-09-15"),
-        ("피자", 13000, 1200, "2026-07-01"),
-        ("짬뽕", 11500, 1000, "2026-10-01"),
-        ("배", 1700, 200, "2026-07-12"),
-        ("대추", 1700, 275, "2026-06-12"),
-        ("복숭아", 8900, 325, "2026-10-12"),
-        ("콜라", 2000, 565, "2027-11-13")
-    ]
-    for name, price, calories, exp_date in foods:
-        refrigerator.append(Food(name=name, price=price, calories=calories, exp_date=date.fromisoformat(exp_date)))
+    food_list = select_list()
+    with open("food_data.json", "w", encoding="utf-8") as f:
+        foods = [
+            ("사과", 1500, 140, "2026-09-01"),
+            ("망고", 4500, 340, "2026-07-21"),
+            ("햄버거", 10000, 950, "2026-09-21"),
+            ("감자샐러드", 7500, 650, "2026-09-15"),
+            ("피자", 13000, 1200, "2026-07-01"),
+            ("짬뽕", 11500, 1000, "2026-10-01"),
+            ("배", 1700, 200, "2026-07-12"),
+            ("대추", 1700, 275, "2026-06-12"),
+            ("복숭아", 8900, 325, "2026-10-12"),
+            ("콜라", 2000, 565, "2027-11-13")
+        ]
+        for name, price, calories, exp_date in foods:
+            food = Food(name=name, price=price, calories=calories, exp_date=date.fromisoformat(exp_date))
+            food_dict = food.to_dict()
+            food_list.append(food_dict)
+        json.dump(food_list, f, ensure_ascii=False, indent=2)
 
     print("--------- 냉장고에 자동 음식 넣기 완료 ---------")
